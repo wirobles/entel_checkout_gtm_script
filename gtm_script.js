46edf0698252684b06f5a3ab2d659e82376bd80f
willy.robles <willy.robles@srburns.pe>
2019-09-10 09:58:31 -0500

    Se realizó evento blur por jquery

--- gtm_script.js
+++ gtm_script.js
@@ -1,4 +1,4 @@
-/*
+
 var array_1 = ['client-email', 'client-first-name', 'client-appat', 'client-apmat', 'client-document', 'client-phone'],
 array_2 = ['client-company-name', 'client-company-document']
 
@@ -7,18 +7,24 @@ document.getElementById('go-to-shipping').disabled = true
 
 // create event to principal fields
 for (var i = 0; i < array_1.length; i++ ) {
-    document.querySelector('#' + array_1[i]).addEventListener('focusout', function(event) {
+    /*document.querySelector('#' + array_1[i]).addEventListener('focusout', function(event) {
         // 
         validateIpunts(array_1)
-    }, true)
+    }, true)*/
+    $('#' + array_1[i]).blur(function() {
+        validateIpunts(array_1)
+    })
 }
 
 // create event to billing fields
 for (var i = 0; i < array_2.length; i++ ) {
-    document.querySelector('#' + array_2[i]).addEventListener('focusout', function(event) {
+    /*document.querySelector('#' + array_2[i]).addEventListener('focusout', function(event) {
         // 
         validateIpunts(array_1.concat(array_2))
-    }, true)
+    }, true)*/
+    $('#' + array_2[i]).blur(function() {
+        validateIpunts(array_1.concat(array_2))
+    })
 }
 
 // create event to checkbox
@@ -54,4 +60,4 @@ function validateInputsClass (array) {
     }
 
     return flag
-}*/
+}
