016590d50959b334632f31e913dc22aed192864b
willy.robles <willy.robles@srburns.pe>
2019-09-10 10:48:53 -0500

    Se encerro requerimiento en una funcion

--- gtm_script.js
+++ gtm_script.js
@@ -1,59 +1,62 @@
+validatingInputs()
 
-var array_1 = ['client-email', 'client-first-name', 'client-appat', 'client-apmat', 'client-document', 'client-phone'],
-array_2 = ['client-company-name', 'client-company-document']
+function validatingInputs () {
+    var array_1 = ['client-email', 'client-first-name', 'client-appat', 'client-apmat', 'client-document', 'client-phone'],
+    array_2 = ['client-company-name', 'client-company-document']
 
-// setting disable to button
-document.getElementById('go-to-shipping').disabled = true
+    // setting disable to button
+    document.getElementById('go-to-shipping').disabled = true
 
-// create event to principal fields
-for (var i = 0; i < array_1.length; i++ ) {
-    $('#' + array_1[i]).blur(function() {
-        validateIpunts(array_1)
-    })
-}
-
-// create event to billing fields
-for (var i = 0; i < array_2.length; i++ ) {
-    $('#' + array_2[i]).blur(function() {
-        validateIpunts(array_1.concat(array_2))
-    })
-}
-
-// create event to checkbox
-document.querySelector('.box-client-info-pj .links').addEventListener('click', function(event) {
-    if ((document.querySelectorAll("#is-corporate-client")[0].style.display === "none") === true ) {
-        // 
-        validateIpunts(array_1.concat(array_2))
-    } else {
-        // 
-        validateIpunts(array_1)
-
-        for (var i = 0; i < array_2.length; i++ ) {
-            document.querySelector('#' + array_2[i]).value = ''
-        }
+    // create event to principal fields
+    for (var i = 0; i < array_1.length; i++ ) {
+        $('#' + array_1[i]).blur(function() {
+            validateIpunts(array_1)
+        })
     }
-})
 
-// Validate inputs
-function validateIpunts (array) {
-    if (validateInputsClass(array)) {
-        document.getElementById('go-to-shipping').disabled = false
-    } else {
-        document.getElementById('go-to-shipping').disabled = true
+    // create event to billing fields
+    for (var i = 0; i < array_2.length; i++ ) {
+        $('#' + array_2[i]).blur(function() {
+            validateIpunts(array_1.concat(array_2))
+        })
     }
-}
 
-// function to validate inputs class
-function validateInputsClass (array) {
-    var flag = true
-    if (true) {
-        for (var i = 0; i < array.length; i++) {
-            var className = document.querySelector('#' + array[i]).getAttribute('class')
-            if (className.indexOf('success') < 0) {
-                flag = false
+    // create event to checkbox
+    document.querySelector('.box-client-info-pj .links').addEventListener('click', function(event) {
+        if ((document.querySelectorAll("#is-corporate-client")[0].style.display === "none") === true ) {
+            // 
+            validateIpunts(array_1.concat(array_2))
+        } else {
+            // 
+            validateIpunts(array_1)
+
+            for (var i = 0; i < array_2.length; i++ ) {
+                document.querySelector('#' + array_2[i]).value = ''
             }
         }
+    })
+
+    // Validate inputs
+    function validateIpunts (array) {
+        if (validateInputsClass(array)) {
+            document.getElementById('go-to-shipping').disabled = false
+        } else {
+            document.getElementById('go-to-shipping').disabled = true
+        }
     }
 
-    return flag
+    // function to validate inputs class
+    function validateInputsClass (array) {
+        var flag = true
+        if (true) {
+            for (var i = 0; i < array.length; i++) {
+                var className = document.querySelector('#' + array[i]).getAttribute('class')
+                if (className.indexOf('success') < 0) {
+                    flag = false
+                }
+            }
+        }
+
+        return flag
+    }
 }
