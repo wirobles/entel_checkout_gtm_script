1804ec540420267a46e6edb8ef159082adbff096
willy.robles <willy.robles@srburns.pe>
2019-09-10 12:49:01 -0500

    Fix

--- gtm_script.js
+++ gtm_script.js
@@ -1,8 +1,8 @@
 validatingInputs()
 
 function validatingInputs () {
-    var array_1 = ['client-email', 'client-first-name', 'client-appat', 'client-apmat', 'client-document', 'client-phone'],
-    array_2 = ['client-company-name', 'client-company-document']
+    var array_1 = ['client-email', 'client-first-name', 'client-appat', 'client-apmat', 'client-document', 'client-phone']
+    var array_2 = ['client-company-name', 'client-company-document']
 
     // setting disable to button
     document.getElementById('go-to-shipping').disabled = true
