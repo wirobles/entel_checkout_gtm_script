5d04ba6910e33d485d653708fa1b94f57d0a6be2
willy.robles <willy.robles@srburns.pe>
2019-09-10 16:56:53 -0500

    Fix

--- gtm_script.js
+++ gtm_script.js
@@ -5,13 +5,6 @@ var array_2 = ['client-company-name', 'client-company-document']
 // setting disable to button
 document.getElementById('go-to-shipping').disabled = true
 
-window.addEventListener("popstate", function() {
-    if (/profile/i.test(window.location.href)) {
-        // disable button
-        document.getElementById('go-to-shipping').disabled = true
-    }
-})
-
 // create event to principal fields
 for (var i = 0; i < array_1.length; i++ ) {
     $('#' + array_1[i]).blur(function() {
