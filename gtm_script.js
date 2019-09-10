b21dd2eb8fa6cd966478e2765c16c2353bf5fdc5
willy.robles <willy.robles@srburns.pe>
2019-09-10 09:40:43 -0500

    Se prueba cambiar evento blur por focusof

--- gtm_script.js
+++ gtm_script.js
@@ -1,5 +1,5 @@
 
-/*var array_1 = ['client-email', 'client-first-name', 'client-appat', 'client-apmat', 'client-document', 'client-phone'],
+var array_1 = ['client-email', 'client-first-name', 'client-appat', 'client-apmat', 'client-document', 'client-phone'],
 array_2 = ['client-company-name', 'client-company-document']
 
 // setting disable to button
@@ -7,51 +7,51 @@ document.getElementById('go-to-shipping').disabled = true
 
 // create event to principal fields
 for (var i = 0; i < array_1.length; i++ ) {
-document.querySelector('#' + array_1[i]).addEventListener('blur', function(event) {
-    // 
-    validateIpunts(array_1)
-}, true)
+    document.querySelector('#' + array_1[i]).addEventListener('focusout', function(event) {
+        // 
+        validateIpunts(array_1)
+    }, true)
 }
 
 // create event to billing fields
 for (var i = 0; i < array_2.length; i++ ) {
-document.querySelector('#' + array_2[i]).addEventListener('blur', function(event) {
-    // 
-    validateIpunts(array_1.concat(array_2))
-}, true)
+    document.querySelector('#' + array_2[i]).addEventListener('focusout', function(event) {
+        // 
+        validateIpunts(array_1.concat(array_2))
+    }, true)
 }
 
 // create event to checkbox
 document.querySelector('.box-client-info-pj .links').addEventListener('click', function(event) {
-if ((document.querySelectorAll("#is-corporate-client")[0].style.display === "none") === true ) {
-    // 
-    validateIpunts(array_1.concat(array_2))
-} else {
-    // 
-    validateIpunts(array_1)
-}
+    if ((document.querySelectorAll("#is-corporate-client")[0].style.display === "none") === true ) {
+        // 
+        validateIpunts(array_1.concat(array_2))
+    } else {
+        // 
+        validateIpunts(array_1)
+    }
 })
 
 // Validate inputs
 function validateIpunts (array) {
-if (validateInputsClass(array)) {
-    document.getElementById('go-to-shipping').disabled = false
-} else {            
-    document.getElementById('go-to-shipping').disabled = true
-}
+    if (validateInputsClass(array)) {
+        document.getElementById('go-to-shipping').disabled = false
+    } else {
+        document.getElementById('go-to-shipping').disabled = true
+    }
 }
 
 // function to validate inputs class
 function validateInputsClass (array) {
-var flag = true
-if (true) {
-    for (var i = 0; i < array.length; i++) {
-        var className = document.querySelector('#' + array[i]).getAttribute('class')
-        if (className.indexOf('success') < 0) {
-            flag = false
+    var flag = true
+    if (true) {
+        for (var i = 0; i < array.length; i++) {
+            var className = document.querySelector('#' + array[i]).getAttribute('class')
+            if (className.indexOf('success') < 0) {
+                flag = false
+            }
         }
     }
-}
 
-return flag
-}*/
+    return flag
+}
