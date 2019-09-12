432e2f6ddbe5a69e1bfa4997b21791697098a2d0
willy.robles <willy.robles@srburns.pe>
2019-09-12 12:05:23 -0500

    Fix

--- gtm_script.js
+++ gtm_script.js
@@ -109,68 +109,70 @@ var interval_fuera_zona_delivery = setInterval(function() {
 // add steps popup html
 window.addEventListener('popstate', function() {
     if (/shipping/i.test(window.location.href)) {
-        var _body = document.querySelector('body'),
-            container = document.createElement('div'),
-            overlay = document.createElement('div'),
-            h5 = document.createElement('h5'),
-            ul = document.createElement('ul'),
-            li1 = document.createElement('li'),
-            li2 = document.createElement('li'),
-            li3 = document.createElement('li'),
-            picture = document.createElement('picture'),
-            source1 = document.createElement('source'),
-            source2 = document.createElement('source'),
-            img = document.createElement('img'),
-            a = document.createElement('a')
+        setTimeout(function(){
+            var _body = document.querySelector('body'),
+                container = document.createElement('div'),
+                overlay = document.createElement('div'),
+                h5 = document.createElement('h5'),
+                ul = document.createElement('ul'),
+                li1 = document.createElement('li'),
+                li2 = document.createElement('li'),
+                li3 = document.createElement('li'),
+                picture = document.createElement('picture'),
+                source1 = document.createElement('source'),
+                source2 = document.createElement('source'),
+                img = document.createElement('img'),
+                a = document.createElement('a')
 
-        container.setAttribute('class','step-popup')
-        overlay.setAttribute('class','popup-overlay')
-        _body.appendChild(container)
-        _body.appendChild(overlay)
-        container.appendChild(h5)
-        container.appendChild(ul)    
-        ul.appendChild(li1)
-        ul.appendChild(li2)
-        ul.appendChild(li3)
-        container.appendChild(picture)
-        picture.appendChild(source1)
-        picture.appendChild(source2)
-        picture.appendChild(img)
-        container.appendChild(a)
-        h5.appendChild(document.createTextNode('Para poder ubicar tu dirección, sigue estos pasos:'))
-        li1.appendChild(document.createTextNode('Ingresa solo el nombre de la calle, avenida o jirón.'))
-        li2.appendChild(document.createTextNode('Recuerda en agregar tu número y distrito.'))
-        li3.appendChild(document.createTextNode('Finalmente, ubica y selecciona tu dirección en la lista.'))
-        source1.setAttribute('media','(min-width: 650px)')
-        source1.setAttribute('srcset','/arquivos/step-popup-img-desktop.jpg')
-        source2.setAttribute('media','(min-width: 465px)')
-        source2.setAttribute('srcset','/arquivos/step-popup-img-desktop.jpg')
-        img.setAttribute('src','/arquivos/step-popup-img.jpg')
-        a.setAttribute('href','javascript:void(0)')
-        a.appendChild(document.createTextNode('Entendido'))
+            container.setAttribute('class','step-popup')
+            overlay.setAttribute('class','popup-overlay')
+            _body.appendChild(container)
+            _body.appendChild(overlay)
+            container.appendChild(h5)
+            container.appendChild(ul)    
+            ul.appendChild(li1)
+            ul.appendChild(li2)
+            ul.appendChild(li3)
+            container.appendChild(picture)
+            picture.appendChild(source1)
+            picture.appendChild(source2)
+            picture.appendChild(img)
+            container.appendChild(a)
+            h5.appendChild(document.createTextNode('Para poder ubicar tu dirección, sigue estos pasos:'))
+            li1.appendChild(document.createTextNode('Ingresa solo el nombre de la calle, avenida o jirón.'))
+            li2.appendChild(document.createTextNode('Recuerda en agregar tu número y distrito.'))
+            li3.appendChild(document.createTextNode('Finalmente, ubica y selecciona tu dirección en la lista.'))
+            source1.setAttribute('media','(min-width: 650px)')
+            source1.setAttribute('srcset','/arquivos/step-popup-img-desktop.jpg')
+            source2.setAttribute('media','(min-width: 465px)')
+            source2.setAttribute('srcset','/arquivos/step-popup-img-desktop.jpg')
+            img.setAttribute('src','/arquivos/step-popup-img.jpg')
+            a.setAttribute('href','javascript:void(0)')
+            a.appendChild(document.createTextNode('Entendido'))
 
-        // elements to click to open steps popup
-        var content_click_to_open_popup = document.querySelectorAll("#ship-address-search")[0],
-            content_click_to_open_popup_p = document.createElement('p'),
-            content_click_to_open_popup_p_a = document.createElement('a')
+            // elements to click to open steps popup
+            var content_click_to_open_popup = document.querySelectorAll("#ship-address-search")[0],
+                content_click_to_open_popup_p = document.createElement('p'),
+                content_click_to_open_popup_p_a = document.createElement('a')
 
-        content_click_to_open_popup.parentElement.appendChild(content_click_to_open_popup_p)
-        content_click_to_open_popup_p.setAttribute('class','btn-dir-here')
-        content_click_to_open_popup_p.appendChild(document.createTextNode('Si no encuentras tu dirección, ingresa '))
-        content_click_to_open_popup_p.appendChild(content_click_to_open_popup_p_a)
-        content_click_to_open_popup_p_a.setAttribute('href','javascript:void(0)')
-        content_click_to_open_popup_p_a.appendChild(document.createTextNode('aquí'))
+            content_click_to_open_popup.parentElement.appendChild(content_click_to_open_popup_p)
+            content_click_to_open_popup_p.setAttribute('class','btn-dir-here')
+            content_click_to_open_popup_p.appendChild(document.createTextNode('Si no encuentras tu dirección, ingresa '))
+            content_click_to_open_popup_p.appendChild(content_click_to_open_popup_p_a)
+            content_click_to_open_popup_p_a.setAttribute('href','javascript:void(0)')
+            content_click_to_open_popup_p_a.appendChild(document.createTextNode('aquí'))
 
-        // events to open steps popup
-        document.querySelector('.btn-dir-here a').addEventListener("click", function() {
-            document.querySelector('.step-popup').classList.add("active")
-            document.querySelector('.popup-overlay').classList.add("active")
-        })
+            // events to open steps popup
+            document.querySelector('.btn-dir-here a').addEventListener("click", function() {
+                document.querySelector('.step-popup').classList.add("active")
+                document.querySelector('.popup-overlay').classList.add("active")
+            })
 
-        document.querySelector('.step-popup a').addEventListener("click", function() {
-            document.querySelector('.step-popup').classList.remove("active")
-            document.querySelector('.popup-overlay').classList.remove("active")
-        })
+            document.querySelector('.step-popup a').addEventListener("click", function() {
+                document.querySelector('.step-popup').classList.remove("active")
+                document.querySelector('.popup-overlay').classList.remove("active")
+            })
+        }, 500)
     }
 })
 /*--------------------------------------------------------------------------------------------------------------------*/
