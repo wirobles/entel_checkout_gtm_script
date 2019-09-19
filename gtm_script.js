b0e162cc9d80eaf773ff8ccf000d7ffa7fe00b2d
willy.robles <willy.robles@srburns.pe>
2019-09-19 11:12:25 -0500

    Se agregó trigger para encuesta de hotjar

--- gtm_script.js
+++ gtm_script.js
@@ -1,3 +1,7 @@
+window.onbeforeunload = function(e) {
+	window.hj('trigger', 'window_closing')
+    return '¡Ayúdanos a mejorar! Cuéntanos el motivo por el cual no completas tu compra:'
+}
 
 /* -------------------------------------- */
 /* -------------------------------------- */
