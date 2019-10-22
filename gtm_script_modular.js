d2d8eea1d1ca54255eb66752ec1595d6d250e429
Gerard Chozo <gerard.chozo@srburns.pe>
2019-10-22 12:39:16 -0500

    Se avanzó con la maquetación para desktop.

---
+++ gtm_script_modular.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('body').find('.e-promo-section__open-button').click(function() {
+        $('.e-promo-section').toggleClass('e-promo-section--state--open')
+    })
+})
