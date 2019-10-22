c460ed559f93427d1515bbe281f8acf43d9c1563
Gerard Chozo <gerard.chozo@srburns.pe>
2019-10-22 17:52:39 -0500

    Se terminó la maquetación.

--- gtm_script_modular.js
+++ gtm_script_modular.js
@@ -1,5 +1,48 @@
 $(document).ready(function() {
-    $('body').find('.e-promo-section__open-button').click(function() {
-        $('.e-promo-section').toggleClass('e-promo-section--state--open')
+    let closePromo,
+        openPromo
+
+    // hearing event
+    $(document).on('click', function (evt) {
+        $(document).trigger('close.promo')
+    })
+
+    // close promo
+    closePromo = function () {
+        $('.e-promo-section').removeClass('e-promo-section--state--open')
+        $('.e-promo-section__open-button').removeClass('active')
+
+        $(document).unbind('close.promo')
+    }
+
+    // open promo
+    openPromo = function () {
+        $('.e-promo-section').addClass('e-promo-section--state--open')
+        $('.e-promo-section__open-button').addClass('active')
+
+        $(document).on('close.promo', function() {
+            closePromo ()
+        })
+    }
+
+    // click promo section event
+    $('body').find('.e-promo-section').click(function (e) {
+        if ($(this).hasClass('e-promo-section--state--open')) {
+            e.stopPropagation()
+            closePromo ()
+        } else {
+            e.stopPropagation()
+            openPromo ()
+        }
+    })
+
+    $('body').find('.open-legal-popup').click(function() {
+        $('.e-promo-overlay').addClass('e-promo-overlay--state--open')
+        $('.e-promo-legales-popup').addClass('e-promo-legales-popup--state--open')
+    })
+
+    $('body').find('.e-promo-legales-popup__close-button, .e-promo-overlay').click(function() {
+        $('.e-promo-overlay').removeClass('e-promo-overlay--state--open')
+        $('.e-promo-legales-popup').removeClass('e-promo-legales-popup--state--open')
     })
 })
