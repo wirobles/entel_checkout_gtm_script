ec733139c7a7f39cdf16239ce311725e5c5533ef
willy.robles <willy.robles@srburns.pe>
2019-11-13 12:00:47 -0500

    Se maquetó boton info am pm

---
+++ gtm_script_aux.js
@@ -0,0 +1,17 @@
+let htmlStringAmPmOverlay = '<div class="popup-overlay"></div>',
+    htmlStringAmPmButton = '<div class="popup-ampm-info-button"><img src="https://entelperu.vteximg.com.br/arquivos/entel-delivery-color.png" alt="Delivery Entel Perú"><p><span class="title">¡Delivery gratis en tiempo record!</span><span class="sub-title">Compra tu celu hoy y recíbelo en menos de 24 horas</span><span class="orange">Más información <span class="cta"></span></span></p></div>',
+    htmlStringAmPmPopup = '<div class="popup-info-delivery"><div class="popup-info-delivery__title"><img src="https://entelperu.vteximg.com.br/arquivos/entel-delivery-color.png" alt="Delivery Entel Perú"><span>¡Delivery gratis en tiempo record!</span></div><div class="popup-info-delivery__subtitle">Compra tu celu hoy y recíbelo <br> en menos de 24 horas</div><div class="popup-info-delivery__orange">¡Aprovecha! haz tu compra por la mañana y recibirás tu pedido por la tarde</div><div class="popup-info-delivery__pay-method"><span>Medios de pago con tarjetas de crédito, débito y efectivo</span><img src="../../images/creditcard-visa.png" alt="Visa"><img src="../../images/creditcard-mastercard.png" alt="Master card"><img src="../../images/creditcard-americanexpress.png" alt="American Express"><img src="../../images/creditcard-dinners.png" alt="Dinners"><img src="../../images/creditcard-pagoefectivo.png" alt="Pago efectivo"></div><div class="popup-info-delivery__text">Condiciones de uso: Para entregas dentro del horario de 3:30pm a 8:00pm del mismo día, la compra y generación de orden debe estar ingresada y confirmada hasta la 1pm. No aplica para distritos de&nbsp;Lurín, Ventanilla, Puente Piedra, Carabayllo, Chosica, Chaclacayo, Ate en Lima. Detalle de cobertura en provincias para ventas por Televentas incluye Trujillo, Trujillo, Victor Larco, Esperanza, Porvenir, Florencia de Mora; en Chiclayo, Chiclayo, La Victoria, Jose Leonardo Ortiz; en Piura, Piura, Castilla, Distrito de 26 de Octubre y Arequipa, Yahahuara, Cercado, Cerro Colorado, Las Flores, Socabaya, Hunter, Jose Luis Bustamante, Mariano Melgar.&nbsp;Los pedidos ingresados desde las 01:0pm en adelante podrán ser programados en los siguientes horarios del día siguiente:&nbsp;8:30am a 12:30pm , 12:30pm a 4:30pm y 4:30 a 8:00pm. Para el siguiente detalle de distritos en provincia aplican horarios específicos de entrega. Piura: Catacaos, La Legua, Simbila, Monte Suyon , solo Martes y Viernes. Piura: Medio Piura, Caserío Miraflores, Río Seco, Los Tejidos, Chapaira – Solo Jueves. Chiclayo: Monsefú – Solo Martes y Viernes. Arequipa: Yahahuara, Cercado, Cerro Colorado, Las Flores, Socabaya, Hunter, Jose Luis Bustamante, Mariano Melgar.</div><span class="popup-info-delivery__button">Entendido</span><span class="popup-info-delivery__close"></span></div>'    
+
+$('.checkout-container.row-fluid').append(htmlStringAmPmOverlay)
+$('.checkout-container.row-fluid').append(htmlStringAmPmPopup)
+$('.checkout-container.row-fluid').append(htmlStringAmPmButton)
+
+$('body').find('.popup-ampm-info-button').click(function () {
+	$('.popup-info-delivery').addClass('active')
+	$('.popup-overlay').addClass('active')
+})
+
+$('body').find('.popup-info-delivery__close,.popup-overlay,.popup-info-delivery__button').click(function () {
+	$('.popup-info-delivery').removeClass('active')
+	$('.popup-overlay').removeClass('active')
+})
